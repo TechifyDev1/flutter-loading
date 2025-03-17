f6607eaeb0c8e81c0fa3de3cd351b62ff0a622ca
TechifyDev <yusufabdulqudus02@gmail.com>
2025-03-17 17:37:50 +0100

    Coding

--- hello.dart
+++ hello.dart
@@ -1,4 +1,15 @@
+void greet(String name) {
+    print("Good morning " + name);
+}
 void main() {
     var myList = [2,3,5,5];
-    print(myList[0]);
+    for (int list in myList) {
+        if (list == 3) {
+          print("Is okay");
+          continue;
+        }
+        list == 5 ? print("object") : print("Subject");
+      print("the current number is $list");
+    }
+    greet("TechifyDev");
 }
